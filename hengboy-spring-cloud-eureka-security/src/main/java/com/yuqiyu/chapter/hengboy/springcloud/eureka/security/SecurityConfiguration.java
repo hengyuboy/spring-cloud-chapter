98bf932e301d44452fed94479d296094c5881f27
恒宇少年 <yuqiyu@vip.qq.com>
2018-09-29 16:12:09 +0800

    SpringCloud组件：你的Eureka服务注册中心安全吗？

--- hengboy-spring-cloud-eureka-security/src/main/java/com/yuqiyu/chapter/hengboy/springcloud/eureka/security/SecurityConfiguration.java
+++ hengboy-spring-cloud-eureka-security/src/main/java/com/yuqiyu/chapter/hengboy/springcloud/eureka/security/SecurityConfiguration.java
@@ -20,8 +20,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    /**
+     * 配置安全信息
+     * - 禁用csrf攻击功能
+     * - 开启所有请求需要验证并且使用http basic进行认证
+     *
+     * @param http
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http.csrf()
                 .disable()
                 .authorizeRequests()
