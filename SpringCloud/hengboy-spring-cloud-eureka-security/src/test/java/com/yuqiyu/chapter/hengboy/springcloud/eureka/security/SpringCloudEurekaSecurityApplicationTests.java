16560c6ca5152765b20f4bec9ce122ca65ef1481
恒宇少年 <jnyuqy@dingtalk.com>
2019-03-01 10:13:34 +0800

    Structural change

---
+++ SpringCloud/hengboy-spring-cloud-eureka-security/src/test/java/com/yuqiyu/chapter/hengboy/springcloud/eureka/security/SpringCloudEurekaSecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yuqiyu.chapter.hengboy.springcloud.eureka.security;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudEurekaSecurityApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
